2866874d497d0651cafef90a1bb8687ea1bdb5d1
Yeah <3790764375@qq.com>
2026-05-07 12:11:57 +0800

    客户端与服务端通信

--- DBMS_Client/DBMS_Client.h
+++ DBMS_Client/DBMS_Client.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
+#include <QTcpSocket>  // 引入 Socket
 #include "ui_DBMS_Client.h"
 
 class DBMS_Client : public QMainWindow
@@ -8,10 +9,15 @@ class DBMS_Client : public QMainWindow
     Q_OBJECT
 
 public:
-    DBMS_Client(QWidget *parent = nullptr);
+    DBMS_Client(QWidget* parent = nullptr);
     ~DBMS_Client();
+
+private slots:
+    void onConnected();
+    void onReadyRead();
+    void onError(QAbstractSocket::SocketError socketError);
 
 private:
     Ui::DBMS_ClientClass ui;
+    QTcpSocket* tcpSocket; // 添加网络套接字指针
 };
-
